9895976ff3524ff6aba3527c3fb263b3434ad87d
Vipul Verma <168752005+alexvipulverma@users.noreply.github.com>
2025-08-03 16:17:06 +0530

    feat: Add functional support (map, filter) to Container; implement in SinglyLinkedList and DoublyLinkedList

--- src/main/java/io/github/sysalexvipulverma/exception/InvalidRangeException.java
+++ src/main/java/io/github/sysalexvipulverma/exception/InvalidRangeException.java
@@ -3,7 +3,8 @@ package io.github.sysalexvipulverma.exception;
 import lombok.NonNull;
 
 public class InvalidRangeException extends IllegalArgumentException {
-    public InvalidRangeException() {}
+    public InvalidRangeException() {
+    }
 
     public InvalidRangeException(@NonNull String msg) {
         super(msg);
